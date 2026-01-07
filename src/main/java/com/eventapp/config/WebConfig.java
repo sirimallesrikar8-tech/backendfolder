d196685e9dd55e0997162a675fe339e9f62fa791
sirimallesrikar8-tech <sirimallesrikar8@gmail.com>
2026-01-07 23:11:18 +0530

    Fix WebConfig to serve uploads with absolute path

--- src/main/java/com/eventapp/config/WebConfig.java
+++ src/main/java/com/eventapp/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry
-            .addResourceHandler("/uploads/**")
-            .addResourceLocations("file:uploads/")
-            .setCachePeriod(3600);
+        // Serve /uploads/** URLs from the absolute uploads folder
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/")
+                .setCachePeriod(3600);
     }
 }
